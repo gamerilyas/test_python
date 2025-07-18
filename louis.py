b38ebebe98f16eec8aa13310f8a13ffca80d3e4c
gamerilyas <ssaciilyas@gmail.com>
2025-07-18 09:44:03 +0200

    #b = "10" #int(b) #print(b)
    nombre = input("Entrez un nombre: ")
    print(nombre)

--- louis.py
+++ louis.py
@@ -1,15 +1,21 @@
 #c=1+1
 #print(c)
-# = 500
-#d(b)
-# = b = c = 5
+#a = 500
+#id(b)
+#a = b = c = 5
 #rint(a, b, c,)
 #print(a, b, c)
-id(True)
-id(True)
-c = True
-id(c)
+#id(True)
+#id(True)
+#c = True
+#id(c)
 # push in github
-
 # git commit -m "first commit"
 # je crée un programme en python
+
+#b = "10"
+#int(b)
+#print(b)
+
+nombre = input("Entrez un nombre: ")
+print(nombre)
