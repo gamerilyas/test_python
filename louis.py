83d668924227b45ce35be280bce984cfb9f89deb
gamerilyas <ssaciilyas@gmail.com>
2025-07-18 09:57:21 +0200

    a = input("Premier nombre : ") b = input("Deuxième nombre : ") c = a + b print("le résultat est "+ c)

--- louis.py
+++ louis.py
@@ -12,10 +12,21 @@
 # push in github
 # git commit -m "first commit"
 # je crée un programme en python
-
 #b = "10"
 #int(b)
 #print(b)
+#nombre = input("Entrez un nombre: ")
+#print(nombre)
 
-nombre = input("Entrez un nombre: ")
-print(nombre)
+#--------------------------------------------------------------------
+
+
+# Demander un premier nombre à l'utilisateur
+# Demander un deuxième nombre à l'utilisateur
+# Additionner les deux nombres ensembles
+# Afficher le résultat à l'utilisateur
+
+a = input("Premier nombre : ")
+b = input("Deuxième nombre : ")
+c = a + b 
+print("le résultat est "+ c)
