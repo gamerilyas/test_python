caa04bc097ae2c870cb5fd2121d6ecb3d8734928
gamerilyas <ssaciilyas@gmail.com>
2025-07-23 19:28:13 +0200

    "9".zfill(4) "9".zfill(3) for i in rage(100) print(i) print(str(i).zfill(4))
    "bonjour".islower
    "Bonjour".islower
    "Bonjour tous le monde".istitle
    "Bonjour Tous Le Monde".istitle
    
    "50".isdigit()
    "a".isdigit()
    "50a".isdigit()
    input()

--- louis.py
+++ louis.py
@@ -33,25 +33,41 @@
 
 #--------------------------------------------------------------------
 
-"Bonjour".upper()
-"Bonjour".lower()
-"bonjour tout le monde".capitalize()
-"bonjour tout le monde".title()
+#"Bonjour".upper()
+#"Bonjour".lower()
+#"bonjour tout le monde".capitalize()
+#"bonjour tout le monde".title()
 
-"bonjour".replace("jour", "soir")
-"bonjour bonjour".remplace("jour", "soir")
-"bonjour bonjour".remplace(" ", "")
-"bonjour bonjour".remplace(" ", "").remplace("jour", "soir")
+#"bonjour".replace("jour", "soir")
+#"bonjour bonjour".remplace("jour", "soir")
+#"bonjour bonjour".remplace(" ", "")
+#"bonjour bonjour".remplace(" ", "").remplace("jour", "soir")
 
-"  bonjour  ".strip()
-"  bon  jour  ".strip()
-"  bonjour  ".strip(" ujor")
-"  bonjour  ".rstrip(" ujor")
-"  bonjour  ".lstrip(" ujor")
+#"  bonjour  ".strip()
+#"  bon  jour  ".strip()
+#"  bonjour  ".strip(" ujor")
+#"  bonjour  ".rstrip(" ujor")
+#"  bonjour  ".lstrip(" ujor")
 
-"1, 2, 3, 4, 5".split(", ")
-", ".join("1, 2, 3, 4, 5".spilt(", "))
-".".join("1, 2, 3, 4, 5".split(", "))
-"-".join(['1', '2', '3'])
+#"1, 2, 3, 4, 5".split(", ")
+#", ".join("1, 2, 3, 4, 5".spilt(", "))
+#".".join("1, 2, 3, 4, 5".split(", "))
+#"-".join(['1', '2', '3'])
+
+#--------------------------------------------------------------------
 
 "9".zfill(4)
+"9".zfill(3)
+for i in rage(100)
+print(i)
+print(str(i).zfill(4))
+
+"bonjour".islower
+"Bonjour".islower
+"Bonjour tous le monde".istitle
+"Bonjour Tous Le Monde".istitle
+
+"50".isdigit()
+"a".isdigit()
+"50a".isdigit()
+input()
